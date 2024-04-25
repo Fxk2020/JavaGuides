aa5c322f6b9cbfc6405e5b926bda810829e7a451
fxk <2629256976@qq.com>
2024-04-25 16:50:11 +0800

    fxk0425

---
+++ springTest/src/main/java/com/example/springtest/sam/Dog.java
@@ -0,0 +1,13 @@
+package com.example.springtest.sam;
+
+public class Dog {
+
+    public void bark(){
+        System.out.println("dog bark!");
+    }
+
+    public void dine(){
+        System.out.println("dog dine!");
+    }
+
+}
